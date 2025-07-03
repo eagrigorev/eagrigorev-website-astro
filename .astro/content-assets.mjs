0ed55a60fe768b0366e92ba2472994ba820de556
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-03 11:55:51 +0300

    Adding PostList component

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_1DRyUn from "src/images/featured-images/test-post-1.jpg?astroContentImageFlag=&importer=mdx%2Fposts%2Ftest-post-1.mdx";
-export default new Map([["src/images/featured-images/test-post-1.jpg?astroContentImageFlag=&importer=mdx%2Fposts%2Ftest-post-1.mdx", __ASTRO_IMAGE_IMPORT_1DRyUn]]);
+import __ASTRO_IMAGE_IMPORT_1DVD74 from "src/images/featured-images/test-post-1.jpg?astroContentImageFlag=&importer=mdx%2Fposts%2Ftest-post-2.mdx";
+export default new Map([["src/images/featured-images/test-post-1.jpg?astroContentImageFlag=&importer=mdx%2Fposts%2Ftest-post-1.mdx", __ASTRO_IMAGE_IMPORT_1DRyUn], ["src/images/featured-images/test-post-1.jpg?astroContentImageFlag=&importer=mdx%2Fposts%2Ftest-post-2.mdx", __ASTRO_IMAGE_IMPORT_1DVD74]]);
 		
