eb02635d860012253e9417054a0e2ff4652c4ffd
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-04 09:11:02 +0300

    mdx testing

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,8 @@
 
 export default new Map([
+["mdx/posts/beyond-the-sea-of-ghosts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Fbeyond-the-sea-of-ghosts.mdx&astroContentModuleFlag=true")],
 ["mdx/posts/test-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-1.mdx&astroContentModuleFlag=true")],
-["mdx/posts/test-post-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-4.mdx&astroContentModuleFlag=true")],
 ["mdx/posts/test-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-2.mdx&astroContentModuleFlag=true")],
-["mdx/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")]]);
+["mdx/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")],
+["mdx/posts/test-post-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-4.mdx&astroContentModuleFlag=true")]]);
 		
