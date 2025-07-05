9a19a23de436b0c34633732645296aa1763e2f4a
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-05 17:13:50 +0300

    More embeds and posts

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,10 @@
 
 export default new Map([
 ["mdx/posts/beyond-the-sea-of-ghosts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Fbeyond-the-sea-of-ghosts.mdx&astroContentModuleFlag=true")],
-["mdx/posts/test-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-1.mdx&astroContentModuleFlag=true")],
-["mdx/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")],
-["mdx/posts/test-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-2.mdx&astroContentModuleFlag=true")],
-["mdx/posts/test-post-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ftest-post-4.mdx&astroContentModuleFlag=true")]]);
+["mdx/posts/early-rains.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Fearly-rains.mdx&astroContentModuleFlag=true")],
+["mdx/posts/crystal-maze.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Fcrystal-maze.mdx&astroContentModuleFlag=true")],
+["mdx/posts/far-lands.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Ffar-lands.mdx&astroContentModuleFlag=true")],
+["mdx/posts/morning-star.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Fmorning-star.mdx&astroContentModuleFlag=true")],
+["mdx/posts/under-apen-himmel.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Funder-apen-himmel.mdx&astroContentModuleFlag=true")],
+["mdx/posts/winde-and-windows.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=mdx%2Fposts%2Fwinde-and-windows.mdx&astroContentModuleFlag=true")]]);
 		
