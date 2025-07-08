69ede8a7e8a0c80b9495ff25d9ff861ac3e85d91
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-08 21:00:01 +0300

    Clean up

--- src/scripts/toggleVisibility.ts
+++
@@ -1,8 +0,0 @@
-export const toggleVisibility = (
-  clickedElementId: string,
-  toggledElementId: string,
-): void => {
-  document.querySelector(clickedElementId)?.addEventListener("click", () => {
-    document.querySelector(toggledElementId)?.classList.toggle("is-hidden");
-  });
-};
