4bf33a4fbce3c9357d471cead6cc961ec78f0ba3
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-15 12:27:43 +0300

    Pagination tweaks

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,13 +1,9 @@
 
 export default new Map([
-["src/content/posts/test-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-2.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")],
+["src/content/posts/test-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-1.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-5.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-7.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-7.mdx&astroContentModuleFlag=true")],
+["src/content/posts/test-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-2.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-4.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-6.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-6.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-9.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-9.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-8.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-8.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-1.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")]]);
 		
