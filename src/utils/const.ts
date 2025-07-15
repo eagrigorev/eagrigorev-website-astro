e638b6f7cb3321ca87bee33aad4ef13f1300f48d
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-15 16:23:40 +0300

    NavigationTop

---
+++ src/utils/const.ts
@@ -0,0 +1,16 @@
+import type { NavigationItem } from "./types";
+
+export const TOP_NAV_ITEMS: NavigationItem[] = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "/about",
+  },
+  {
+    title: "Contact",
+    url: "/contact",
+  },
+];
