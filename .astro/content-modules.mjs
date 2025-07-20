aa647f689f8a5042d5edd4d6f56717a521f650c5
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-20 18:13:35 +0300

    Linting

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-1.mdx&astroContentModuleFlag=true")],
+["src/content/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-2.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-5.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-4.mdx&astroContentModuleFlag=true")],
