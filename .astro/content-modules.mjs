995db9c63a6886d60471f2f45446ddece5c51904
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-20 18:08:48 +0300

    Tweaks

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
+["src/content/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-1.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-5.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-6.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-6.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-4.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/test-post-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-2.mdx&astroContentModuleFlag=true")],
-["src/content/posts/test-post-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-3.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/test-post-5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-5.mdx&astroContentModuleFlag=true")],
+["src/content/posts/test-post-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-4.mdx&astroContentModuleFlag=true")],
+["src/content/posts/test-post-6.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftest-post-6.mdx&astroContentModuleFlag=true")]]);
 		
