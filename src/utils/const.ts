f8b10239543f4c87a39bc40acf03e55f876655d5
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-07-23 14:44:54 +0300

    GLobalWrapper tweaks

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,50 +1,5 @@
 import type { NavigationItem, PageTitle } from "./types";
 
-export const TOP_NAV_ITEMS: NavigationItem[] = [
-  {
-    title: "Home",
-    url: "/",
-  },
-  {
-    title: "About",
-    url: "/about",
-  },
-  {
-    title: "Contact",
-    url: "/contact",
-  },
-];
-
-export const FOOTER_NAV_ITEMS: NavigationItem[] = [
-  {
-    title: "RSS Feed",
-    url: "/",
-  },
-  {
-    title: "Privacy & Security",
-    url: "/privacy-and-security",
-  },
-  {
-    title: "Sitemap",
-    url: "/",
-  },
-];
-
-export const SOCIAL_LINKS: NavigationItem[] = [
-  {
-    title: "Instagram",
-    url: "https://www.instagram.com/eagrigorev/",
-  },
-  {
-    title: "Bluesky",
-    url: "https://bsky.app/profile/eagrigorev.bsky.social/",
-  },
-  {
-    title: "GitHub",
-    url: "https://github.com/eagrigorev/",
-  },
-];
-
 export const INDEX_PAGE_TITLE: PageTitle = {
   heading: "I'm a frontend software developer based in Thessaloniki, Greece.",
   subheading:
