d48f39d3646f4d0a9279a4c50788e3bf1e2b7ef8
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-08-10 10:47:53 +0300

    Markdown wrapper tweaks

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,8 +1,8 @@
 
-import __ASTRO_IMAGE_IMPORT_1mJCLD from "@images/25.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy.mdx";
-import __ASTRO_IMAGE_IMPORT_ZIrdBG from "@images/34.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+2.mdx";
 import __ASTRO_IMAGE_IMPORT_1ukDxl from "@images/32.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+3.mdx";
+import __ASTRO_IMAGE_IMPORT_ZIrdBG from "@images/34.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+2.mdx";
+import __ASTRO_IMAGE_IMPORT_1mJCLD from "@images/25.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy.mdx";
 import __ASTRO_IMAGE_IMPORT_Z1vGnro from "@images/26.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+4.mdx";
 import __ASTRO_IMAGE_IMPORT_17EOLm from "@images/6.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3.mdx";
-export default new Map([["@images/25.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy.mdx", __ASTRO_IMAGE_IMPORT_1mJCLD], ["@images/34.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+2.mdx", __ASTRO_IMAGE_IMPORT_ZIrdBG], ["@images/32.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+3.mdx", __ASTRO_IMAGE_IMPORT_1ukDxl], ["@images/26.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+4.mdx", __ASTRO_IMAGE_IMPORT_Z1vGnro], ["@images/6.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3.mdx", __ASTRO_IMAGE_IMPORT_17EOLm]]);
+export default new Map([["@images/32.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+3.mdx", __ASTRO_IMAGE_IMPORT_1ukDxl], ["@images/34.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+2.mdx", __ASTRO_IMAGE_IMPORT_ZIrdBG], ["@images/25.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy.mdx", __ASTRO_IMAGE_IMPORT_1mJCLD], ["@images/26.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3+copy+4.mdx", __ASTRO_IMAGE_IMPORT_Z1vGnro], ["@images/6.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftest-post-3.mdx", __ASTRO_IMAGE_IMPORT_17EOLm]]);
 		
