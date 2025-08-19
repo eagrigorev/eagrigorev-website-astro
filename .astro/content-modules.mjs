4551a740a55285ac68918650b2b42e77f6aa0bd7
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-08-19 18:55:30 +0300

    Adding sitemap and rss

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
-["src/content/posts/norwegian-church-at-cardiff-bay-in-march.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fnorwegian-church-at-cardiff-bay-in-march.mdx&astroContentModuleFlag=true")],
-["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/a-kitchen-in-athens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fa-kitchen-in-athens.mdx&astroContentModuleFlag=true")],
+["src/content/posts/norwegian-church-at-cardiff-bay-in-march.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fnorwegian-church-at-cardiff-bay-in-march.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/exploring-the-acropolis-of-athens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fexploring-the-acropolis-of-athens.mdx&astroContentModuleFlag=true")],
-["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")]]);
+["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")],
+["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")]]);
 		
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: "https://www.eagrigorev.com",
+  integrations: [mdx(), sitemap()],
   redirects: {
     "/posts": "/",
     "/tags": "/",
