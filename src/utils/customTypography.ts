aa5b362c0087f67ec37570a89e23575a8e5a9d58
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-08-20 15:47:21 +0300

    Small refactoring

---
+++ src/utils/customTypography.ts
@@ -0,0 +1,11 @@
+import Heading2 from "@components/typography/Heading2.astro";
+import Heading3 from "@components/typography/Heading3.astro";
+import Heading4 from "@components/typography/Heading4.astro";
+import Link from "@components/typography/Link.astro";
+
+export const customTypography = {
+  h2: Heading2,
+  h3: Heading3,
+  h4: Heading4,
+  a: Link,
+};
