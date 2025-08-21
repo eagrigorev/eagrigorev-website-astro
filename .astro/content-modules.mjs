34190b9454eb954162bb7f4be30599966bae54f6
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-08-21 15:11:23 +0300

    New templates

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,9 +2,9 @@
 export default new Map([
 ["src/content/posts/a-kitchen-in-athens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fa-kitchen-in-athens.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/exploring-the-acropolis-of-athens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fexploring-the-acropolis-of-athens.mdx&astroContentModuleFlag=true")],
-["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/norwegian-church-at-cardiff-bay-in-march.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fnorwegian-church-at-cardiff-bay-in-march.mdx&astroContentModuleFlag=true")],
+["src/content/pages/colophon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fcolophon.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")],
-["src/content/pages/privacy-and-security.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fprivacy-and-security.mdx&astroContentModuleFlag=true")],
-["src/content/pages/colophon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fcolophon.mdx&astroContentModuleFlag=true")]]);
+["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
+["src/content/pages/privacy-and-security.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fprivacy-and-security.mdx&astroContentModuleFlag=true")]]);
 		
