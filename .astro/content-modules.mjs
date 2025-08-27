e6eaf4786c9f7903414063112c40cbb18fee0e7b
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-08-27 13:58:27 +0300

    Posting Hills of Bath, Somerset

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,17 +1,18 @@
 
 export default new Map([
 ["src/content/posts/a-kitchen-in-athens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fa-kitchen-in-athens.mdx&astroContentModuleFlag=true")],
-["src/content/posts/exploring-the-acropolis-of-athens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fexploring-the-acropolis-of-athens.mdx&astroContentModuleFlag=true")],
-["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/norwegian-church-at-cardiff-bay-in-march.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fnorwegian-church-at-cardiff-bay-in-march.mdx&astroContentModuleFlag=true")],
+["src/content/pages/privacy-and-security.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fprivacy-and-security.mdx&astroContentModuleFlag=true")],
+["src/content/posts/exploring-the-acropolis-of-athens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fexploring-the-acropolis-of-athens.mdx&astroContentModuleFlag=true")],
+["src/content/posts/hills-of-bath-somerset.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fhills-of-bath-somerset.mdx&astroContentModuleFlag=true")],
+["src/content/pages/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fabout.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/colophon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fcolophon.mdx&astroContentModuleFlag=true")],
 ["src/content/pages/now.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fnow.mdx&astroContentModuleFlag=true")],
-["src/content/pages/privacy-and-security.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpages%2Fprivacy-and-security.mdx&astroContentModuleFlag=true")],
-["src/content/music/crystal-maze.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Fcrystal-maze.mdx&astroContentModuleFlag=true")],
-["src/content/music/morning-star.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Fmorning-star.mdx&astroContentModuleFlag=true")],
+["src/content/music/far-lands.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Ffar-lands.mdx&astroContentModuleFlag=true")],
 ["src/content/music/early-rains.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Fearly-rains.mdx&astroContentModuleFlag=true")],
 ["src/content/music/beyond-the-sea-of-ghosts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Fbeyond-the-sea-of-ghosts.mdx&astroContentModuleFlag=true")],
-["src/content/music/far-lands.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Ffar-lands.mdx&astroContentModuleFlag=true")],
-["src/content/music/winds-and-windows.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Fwinds-and-windows.mdx&astroContentModuleFlag=true")],
-["src/content/music/under-apen-himmel.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Funder-apen-himmel.mdx&astroContentModuleFlag=true")]]);
+["src/content/music/morning-star.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Fmorning-star.mdx&astroContentModuleFlag=true")],
+["src/content/music/crystal-maze.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Fcrystal-maze.mdx&astroContentModuleFlag=true")],
+["src/content/music/under-apen-himmel.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Funder-apen-himmel.mdx&astroContentModuleFlag=true")],
+["src/content/music/winds-and-windows.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmusic%2Fwinds-and-windows.mdx&astroContentModuleFlag=true")]]);
 		
