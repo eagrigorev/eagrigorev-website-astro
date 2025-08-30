190715103d71bc7590dee025c6ea701f15c885ab
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-08-30 11:54:29 +0300

    Small fixes

--- src/content.config.ts
+++ src/content.config.ts
@@ -32,23 +32,6 @@ const pages = defineCollection({
   }),
 });
 
-// const music = defineCollection({
-//   loader: glob({
-//     pattern: "**/*.{md,mdx}",
-//     base: "./src/content/music",
-//   }),
-//   schema: ({ image }) =>
-//     z.object({
-//       title: z.string(),
-//       externalLink: z.string(),
-//       datePublished: z.string(),
-//       image: z.object({
-//         url: image(),
-//         alt: z.string(),
-//       }),
-//     }),
-// });
-
 const albums = defineCollection({
   loader: file("src/data/albums.json"),
   schema: ({ image }) =>
