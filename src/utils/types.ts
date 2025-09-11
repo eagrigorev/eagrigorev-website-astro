9458dcbe0e1c7b7b0c355c5ea3899dfbbe6be2fd
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-09-11 13:28:35 +0300

    Page title component (desktop-only)

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,19 @@ export interface NavigationItem {
   title: string;
   url: string;
 }
+
+export interface PageTitleOptions {
+  heading: string;
+  subheading: string;
+  image: {
+    isVisible: boolean;
+  };
+  categories: {
+    areVisible: boolean;
+  };
+  meta: {
+    isVisible: boolean;
+    datePublished?: string;
+    category?: string;
+  };
+}
