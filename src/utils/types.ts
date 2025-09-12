cd2d27bf72808ac4004f492b52752c393b351b5b
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-09-12 22:47:21 +0300

    PageTitle component

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,15 @@ export interface NavigationItem {
   title: string;
   url: string;
 }
+
+export interface Title {
+  heading: string;
+  subheading: string;
+  hasImage: boolean;
+  hasCategories: boolean;
+  hasMeta: boolean;
+  meta?: {
+    datePublished?: string;
+    category?: string;
+  };
+}
