9ada093032453a8b1f8de02d2b915eb880971bb2
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-11-01 13:57:43 +0200

    CLEANUP BEFORE REDESIGN

--- src/utils/customTypography.ts
+++
@@ -1,9 +0,0 @@
-import Heading2 from "@components/typography/Heading2.astro";
-import Heading3 from "@components/typography/Heading3.astro";
-import Link from "@components/typography/Link.astro";
-
-export const customTypography = {
-  h2: Heading2,
-  h3: Heading3,
-  a: Link,
-};
