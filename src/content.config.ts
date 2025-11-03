81ef558efcb4e8043edac14deb3eb9950f9e77ee
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-11-03 20:16:33 +0200

    Logo component

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,23 +1,23 @@
-// import { glob, file } from "astro/loaders";
-// import { z, defineCollection } from "astro:content";
+import { glob, file } from "astro/loaders";
+import { z, defineCollection } from "astro:content";
 
-// const posts = defineCollection({
-//   loader: glob({
-//     pattern: "**/*.{md,mdx}",
-//     base: "./src/content/posts",
-//   }),
-//   schema: ({ image }) =>
-//     z.object({
-//       title: z.string(),
-//       slug: z.string(),
-//       datePublished: z.string(),
-//       featuredImage: z.object({
-//         url: image(),
-//         alt: z.string(),
-//       }),
-//       tags: z.array(z.string()),
-//     }),
-// });
+const posts = defineCollection({
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/posts",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      slug: z.string(),
+      datePublished: z.string(),
+      featuredImage: z.object({
+        url: image(),
+        alt: z.string(),
+      }),
+      tags: z.array(z.string()),
+    }),
+});
 
 // export const collections = {
 //   posts,
