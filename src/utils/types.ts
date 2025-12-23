6fa60caf48fd7835d28295c209ce31c771983a17
Evgenii Grigorev <eagrigorev87@gmail.com>
2025-12-23 15:50:53 +0200

    Feed adjustments

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,5 +5,11 @@ export interface NavigationItem {
   url: string;
 }
 
+export interface TagItem {
+  title: string;
+  url: string;
+  count: number;
+}
+
 export type Post = CollectionEntry<"posts">;
 export type Page = CollectionEntry<"pages">;
