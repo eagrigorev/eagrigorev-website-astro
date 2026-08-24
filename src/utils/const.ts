8861018cda74ca604bbc6ee6145f1dc06fca6719
Evgenii Grigorev <eagrigorev87@gmail.com>
2026-08-24 16:03:51 +0300

    Updating SiteWrapper and Metadata

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,4 @@
-export enum WEBSITE {
+export enum SITE {
   AUTHOR = "Evgenii Grigorev",
   TITLE = "Gardens & Ruins",
   DESCRIPTION = "Personal logs, film photography fragments, and digital artifacts",
