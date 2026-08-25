cbc2fd08b0910bb0a0b37fa7d1e507d4338d7d8d
Evgenii Grigorev <eagrigorev87@gmail.com>
2026-08-25 15:01:04 +0300

    PostCard and Grid implementation

---
+++ src/scripts/getPosts.ts
@@ -0,0 +1,23 @@
+import type { Post } from "@utils/types";
+import { getCollection } from "astro:content";
+import { sortPostsDesc } from "@scripts/common";
+
+export const getSortedPosts = async (): Promise<Post[]> => {
+  const posts: Post[] = await getCollection(
+    "posts",
+    ({ data }) => !data.isDraft,
+  ).then((response) => sortPostsDesc(response));
+  return posts;
+};
+
+export const getFeaturedPosts = async (): Promise<Post[]> => {
+  const posts: Post[] = await getSortedPosts();
+  return posts.filter((post: Post) => post.data.isFeatured).slice(0, 6);
+};
+
+// TODO: Filter out featured posts if needed
+
+export const getRecentPosts = async (): Promise<Post[]> => {
+  const posts: Post[] = await getSortedPosts();
+  return posts.slice(0, 10);
+};
